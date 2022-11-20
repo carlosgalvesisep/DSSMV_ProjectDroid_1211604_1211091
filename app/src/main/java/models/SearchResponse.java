ed56d7a945cd9c451cfd16e38844da46c5e6c083
carlosgalvesisep <1211604@isep.ipp.pt>
2022-11-20 20:38:03 +0000

    Implementação da pesquisa direta por receitas

---
+++ app/src/main/java/models/SearchResponse.java
@@ -0,0 +1,10 @@
+package models;
+
+import java.util.ArrayList;
+
+public class SearchResponse {
+    public ArrayList<Result> results;
+    public int offset;
+    public int number;
+    public int totalResults;
+}
